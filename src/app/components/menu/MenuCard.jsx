3e19396c608b0e48cb9088d0f4a7a1b1c0bad742
Eismer Lobaina <eismerlobaina@gmail.com>
2023-07-27 16:20:21 -0400

    FEAT: Implementado el menu

--- src/app/components/menu/MenuCard.jsx
+++ src/app/components/menu/MenuCard.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { quicksand } from "@/fonts/quicksand";
+import { gilda_display } from "@/fonts/gilda_display";
+
 
 export const MenuCard = ({
   category,
@@ -13,11 +16,14 @@ export const MenuCard = ({
 }) => {
   //console.log(name);
   return (
-    <div className=" lg:basis-1/2 basis-full p-5 text-center">
-      <div className="flex flex-row">
-        <p className="basis-1/2">{name}</p>
-        <p className="basis-1/2 text-end text-bermuda">{price}</p>
+   
+      <div className=" lg:basis-1/2 basis-full p-5 text-center">
+        <div className="flex flex-row mb-4">
+          <p className={`${quicksand.className} "basis-1/2 text-5"`}>{name}</p>
+          <p className={`${gilda_display.className} basis-1/2 text-end text-green-600`}>{price}</p>
+        </div>
+        <hr />
       </div>
-    </div>
+  
   );
 };
