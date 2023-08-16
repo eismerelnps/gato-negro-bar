1c3a8fc0e91035b809c577f7eaefbbf61fac0403
Eismer Lobaina <eismerlobaina@gmail.com>
2023-08-16 00:29:17 -0400

    FIX: #EN layout del menu corregidos disenno

--- src/app/menu/layout.js
+++ src/app/menu/layout.js
@@ -1,15 +1,12 @@
 import Image from "next/image";
 
+import cover from "../../../public/cover.jpg";
+
 export default async function page({ children }) {
   return (
-    <div className="mt-16">
+    <div className="mt-16 bg-white ">
       <div className="flex justify-center">
-        <Image
-          src={"/cover.jpg"}
-          alt="gato-negro-image"
-          width={500}
-          height={500}
-        />
+        <Image src={cover} alt="gato-negro-image" width={500} height={500} />
       </div>
       {children}
     </div>
